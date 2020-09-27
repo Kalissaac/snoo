49c0e436c374cc50498f765ff195c8f9bf277b0e
Kalissaac <kalissaac@protonmail.com>
2020-09-27 11:48:55 -0700

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,64 @@
+import Head from 'next/head'
+
+export default function Page() {
+  const statusCode = '404'
+  const title = 'This page could not be found'
+  return (
+    <div style={styles.error}>
+      <Head>
+        <title>
+          {statusCode}: {title}
+        </title>
+      </Head>
+      <div>
+        <style dangerouslySetInnerHTML={{ __html: 'body { margin: 0 }' }} />
+        {statusCode ? <h1 style={styles.h1}>{statusCode}</h1> : null}
+        <div style={styles.desc}>
+          <h2 style={styles.h2}>{title}.</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const styles: { [k: string]: React.CSSProperties } = {
+  error: {
+    color: '#000',
+    background: '#fff',
+    fontFamily:
+      'Poppins, -apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif',
+    height: '100vh',
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  desc: {
+    display: 'inline-block',
+    textAlign: 'left',
+    lineHeight: '49px',
+    height: '49px',
+    verticalAlign: 'middle',
+  },
+
+  h1: {
+    display: 'inline-block',
+    borderRight: '1px solid rgba(0, 0, 0,.3)',
+    margin: 0,
+    marginRight: '20px',
+    padding: '10px 23px 10px 0',
+    fontSize: '24px',
+    fontWeight: 500,
+    verticalAlign: 'top',
+  },
+
+  h2: {
+    fontSize: '14px',
+    fontWeight: 'normal',
+    lineHeight: 'inherit',
+    margin: 0,
+    padding: 0,
+  },
+}
