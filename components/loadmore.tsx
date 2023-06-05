2f1bccd9adb4171d37a35c3e2b91ae2feac43fb7
Kalissaac <kalissaac@protonmail.com>
2023-06-05 06:55:24 -0700

    Fix load more button background color in dark mode

--- components/loadmore.tsx
+++ components/loadmore.tsx
@@ -8,7 +8,7 @@ export default function LoadMoreButton({
   return (
     <button
       onClick={() => setSize(s => s + 1)}
-      className='w-full p-2 text-gray-600 transition-opacity border border-gray-200 rounded-md shadow-sm bg-gray-50 hover:opacity-80'
+      className='w-full p-2 text-gray-600 transition-opacity border border-gray-200 rounded-md shadow-sm bg-gray-50 dark:bg-darker-gray hover:opacity-80'
       disabled={isLoading}
     >
       {isLoading ? 'loading...' : 'load more posts'}
